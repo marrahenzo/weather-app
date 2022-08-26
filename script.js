adf035a85e4fa20ece2723a0b4ebe0f0a2a17245
Hernán Marrapodi <hernanmarra2002@gmail.com>
2022-08-26 15:39:11 -0300

    Add updateDOM function to propely display fetched data

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 let weatherData;
 let search = 'london';
+let currentUnit = 'C';
+
+//DOM element references
+
 const searchButton = document.querySelector('#form-search');
 const searchInput = document.querySelector('#search');
+const regionText = document.querySelector('#location');
+const temperatureNumber = document.querySelector('#temperature-number');
+const temperatureUnit = document.querySelector('#temperature-unit');
+const feelsLikeText = document.querySelector('#feels-like');
+const maxTempText = document.querySelector('#max-temp');
+const minTempText = document.querySelector('#min-temp');
+const windText = document.querySelector('#wind');
+const humidityText = document.querySelector('#humidity');
 
 //If a search was done, gather info from url
 
@@ -19,6 +31,7 @@ async function getData(search) {
     weatherData = {
       name: data.name,
       weather: data.weather[0].main,
+      weatherDescription: data.weather[0].description,
       temp: data.main,
       wind: data.wind,
       sunrise: data.sys.sunrise,
@@ -30,6 +43,20 @@ async function getData(search) {
     }
   }
   console.log(weatherData);
+  updateDOM(weatherData);
+}
+
+//Put api data into DOM elements
+
+function updateDOM(data) {
+  regionText.textContent = data.name;
+  temperatureNumber.textContent = data.temp.temp;
+  temperatureUnit.textContent = `°${currentUnit}`;
+  feelsLikeText.textContent = `Feels like: ${data.temp.feels_like}°${currentUnit}`;
+  maxTempText.textContent = `Max: ${data.temp.temp_max}`;
+  minTempText.textContent = `Min: ${data.temp.temp_min}`;
+  windText.textContent = `Wind: ${data.wind.speed} km/h`;
+  humidityText.textContent = `Humidity: ${data.temp.humidity}%`;
 }
 
 getData(search);
